e46939b0f33d329df1c350483b543b13807f072b
EdwardGoomba <edward@dock90.io>
2020-04-09 15:31:42 -0400

    Setup page for netlify deployment

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+
+import styled from 'styled-components'
+
+const Container = styled.div`
+  background: #f7f9fc;
+`
+
+const Index = () => {
+  return (
+    <Container>
+      <h1>Firefly Code School</h1>
+      <h2>Become a web developer from scratch.</h2>
+      <p>Contact: edward@fireflycodeschool.com</p>
+    </Container>
+  )
+}
+
+export default Index
